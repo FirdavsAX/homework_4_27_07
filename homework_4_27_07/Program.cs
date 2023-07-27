d18c0b7878cb6d59eb17fb7c6d52590a29ec921c
FirdavsAX <firdavsjumayev320@gmail.com>
2023-07-27 14:24:32 +0500

    Complete default result

--- homework_4_27_07/Program.cs
+++ homework_4_27_07/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter the array size : ");
+
+            int.TryParse(Console.ReadLine(), out int size);
+
+            int[] arr = new int[size];
+            int[] helpArr = new int[size];
+
+            for (int i = 0; i<size; i++)
+            {
+                int.TryParse(Console.ReadLine(), out arr[i]);
+            }
+
+            Console.Write("Enter position split of array : ");
+
+            int.TryParse(Console.ReadLine(), out int pos);
+
+            for (int i = pos+1 ; i < size; i++) 
+            {
+                helpArr[i - pos -1] = arr[i];
+            }
+            for (int i = 0; i <= pos; i++) 
+            {
+                helpArr[size-pos-1+i] = arr[i];
+            }
+
+            foreach (int i in helpArr) 
+            {
+                Console.WriteLine(i+" ") ;
+            }
         }
     }
 }
